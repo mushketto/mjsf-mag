d33390ec06a683e71bbf9b282211fd5aa9018f03
mushketto <151857764+mushketto@users.noreply.github.com>
2026-09-14 15:03:23 +0300

    Лабораторна робота 1: Vue у Docker

---
+++ lab1/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import heroImg from '../assets/hero.png'
+import viteLogo from '../assets/vite.svg'
+import vueLogo from '../assets/vue.svg'
+
+const count = ref(0)
+</script>
+
+<template>
+  <section id="center">
+    <div>
+      <h1>Беркман Дмитро Валерійович</h1>
+      <p>Інженерія програмного забезпечення</p>
+      <p>2 курс магістратури, група 7.F2.25-2</p>
+    </div>
+  </section>
+
+</template>
